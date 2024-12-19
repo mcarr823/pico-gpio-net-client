ac7b5a47e74881a6e7ffd5f658761f3c462b96ea
mcarr823 <136939846+mcarr823@users.noreply.github.com>
2024-12-19 18:29:05 +1100

    Initial project creation

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    alias(libs.plugins.kotlin.jvm)
+}
+
+group = "dev.mcarr"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(21)
+}
+
