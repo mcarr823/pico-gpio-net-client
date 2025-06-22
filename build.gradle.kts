55c01a68919b8cbaea9d3d8ddaa37ece09127dcd
mcarr823 <136939846+mcarr823@users.noreply.github.com>
2025-06-22 19:52:07 +1000

    Added dokka for javadoc publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.androidLibrary) apply false
-    alias(libs.plugins.kotlinMultiplatform) apply  false
+    alias(libs.plugins.kotlinMultiplatform) apply false
     alias(libs.plugins.vanniktech.mavenPublish) apply false
+    alias(libs.plugins.dokka) apply false
 }
